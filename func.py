f9e357f4fc5db238bd1215ff614efd336d67298e
Ableenk <58062906+Ableenk@users.noreply.github.com>
2020-06-10 16:35:45 +0300

    Add files via upload

---
+++ func.py
@@ -0,0 +1,53 @@
+from urllib import request
+from bs4 import BeautifulSoup as bs
+from collections import Counter
+
+
+
+
+def htmltotext(html):
+    text = html.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+def cleaning(s):
+    res = ''
+    punctuationordigits = ['.', ',', ':', ';', '!', '?', '(', ')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', "«", "»", "&"]
+    for i in range(len(s)):
+        if not (s[i] in punctuationordigits):
+            res += s[i]
+    return res.lower()
+
+def scriptout(html):
+    for script in html(["script", "style"]):
+        script.extract()
+
+
+
+
+def get_html(num):
+    url = 'https://habr.com/ru/post/' + str(num) + '/'
+    html = request.urlopen(url).read()
+    html = bs(html, features="lxml")
+    return html
+
+def writing(s):
+    with open('compressor.txt', 'a', encoding='UTF-8') as output:
+        output.write(s + '\n')
+
+def get_words(start, end, event_for_wait, event_for_set):
+    max_page = 506123
+    event_for_wait.wait()
+    event_for_wait.clear()
+    for i in range(start, end):
+        try:
+            html = get_html(i)
+            scriptout(html)
+            text = htmltotext(html)
+            writing(cleaning(text))
+        except Exception:
+            pass
+    event_for_set.set()
+
---
+++ main.py
@@ -0,0 +1,51 @@
+import threading
+from func import get_words
+from collections import Counter
+from string import ascii_letters
+
+def topten(mas):
+    for i in range(10):
+        print(str(i+1)+')', mas[i][0])
+
+def format(mas):
+    pretexesnmonthes = list('без, перед, при, через, над, под, про, для, что, как, где, зачем, августа, июля, июня, сентября, октября, ноября, декабря, января, февраля, марта, апреля, мая'.split(', '))
+    for i in mas:
+        if (len(i) <= 2) or validate(i) or i in pretexesnmonthes:
+            mas[i] = 0
+    return mas.most_common(10)
+
+def validate(nickname):
+    return all(map(lambda c: c in ascii_letters, nickname))
+
+def filezation():
+    s = ''
+    f = open('compressor.txt', encoding='UTF-8')
+    for line in f:
+        s += line
+    f.close()
+    open('compressor.txt', 'w')
+    f.close()
+    return s
+
+period = 10
+
+e1 = threading.Event()
+e2 = threading.Event()
+e3 = threading.Event()
+
+t1 = threading.Thread(target=get_words, args=(1, period, e1, e2))
+t2 = threading.Thread(target=get_words, args=(period, 2*period, e2, e3))
+t3 = threading.Thread(target=get_words, args=(2*period, 3*period, e3, e1))
+
+t1.start()
+t2.start()
+t3.start()
+
+e1.set()
+
+t1.join()
+t2.join()
+t3.join()
+
+words = Counter(filezation().split())
+topten(format(words))
